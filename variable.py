fa95e6624cb627e4af4cffa37cc49892f8638352
Kamrul Hasan <kamrulcse7@gmail.com>
2023-03-08 23:51:30 +0600

    python variable

---
+++ variable.py
@@ -0,0 +1,64 @@
+# id = 5 #int type 
+# name = "kamrul hasan" #str type
+# print(id)
+# print(name)
+
+
+# #type casting
+# x = str(3) #int to string
+# y = int(3.0) #float to int
+# z = float(120) #int to float
+# print(x)
+# print(y)
+# print(z)
+
+# #get variable type
+# print(type(x))
+
+# # Legal variable names:
+# myvar = "John"
+# my_var = "John"
+# _my_var = "John"
+# myVar = "John"
+# MYVAR = "John"
+# myvar2 = "John"
+
+# # Camel Case
+# myVariableName = "John"
+# # Pascal Case
+# MyVariableName = "John"
+# # Snake Case
+# my_variable_name = "John"
+
+# # Many Values to Multiple Variables
+# id, name, email = 37, "kamrul", "kamrulcse7@gmail.com"
+# print(id)
+# print(name)
+# print(email)
+# print(str(id) +' '+ name + ' ' + email)
+
+# #Unpack a Collection
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+# # or
+# print(x, y, z)
+# # or
+# print(x + y + z)
+
+# a = 5
+# b = "kamrul"
+# print(a, b)
+# # or
+# print(str(a) + ' ' + b)
+
+
+# Python - Global Variables
+name = "kamrul hasan"
+#create function
+def myFun():
+    print("My Name is " + name)
+
+myFun() # call function
