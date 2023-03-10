3e1600214f2ecef9f899f35a20f000a458ea1273
Kamrul Hasan <kamrulcse7@gmail.com>
2023-03-10 20:40:38 +0600

    data type

---
+++ data_type.py
@@ -0,0 +1,21 @@
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+
+name = "kamrul" #string
+id = 37 #integer
+gpa = 3.67 #float
+x = 1j #complex
+nameList = ["kamrul", "arafat", "rakib"] #list
+nameTuple = ("kamrul", "hasan", "mahmud") #tuple
+n = range(10) #range output: range(0, 10)
+info = {"name" : "kamrul hasan","age" : 26,"email" : "kamrulcse7@gmail.com"} #dect(dart map)
+# print(info['name'])
+click = True
+print(click)
